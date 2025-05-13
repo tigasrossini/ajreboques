1d718008ef70df25eda1bf6bfcb35c484ea1784f
MatheusJeff <matheussjp.fernandes19@gmail.com>
2025-05-13 14:43:22 -0300

    Sistema de redefinição de senha

--- packages/backend/routes/web.php
+++ packages/backend/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Facades\Route;
 use App\Mail\BemVindoMail;
 use Illuminate\Support\Facades\Mail;
@@ -8,9 +10,14 @@ use Illuminate\Support\Facades\Mail;
 Route::get('/email', function(){
     $user = new \stdClass();
     $user -> name = 'Usuario teste';
-    $user -> email = 'tiagorossinicorrea@gmail.com';
+    $user -> email = 'pewipiw950@inkight.com';
 
     Mail::to($user->email)->send(new BemVindoMail);
 
     return 'Email criado com sucesso';
+});
+
+
+Route::get('teste', function(){
+    return view('auth.reset-password');
 });
