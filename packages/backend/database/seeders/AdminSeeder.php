1f09a4ae178bc80ae857e1e2ef0c6603bd6891b3
MatheusJeff <matheussjp.fernandes19@gmail.com>
2025-06-13 23:18:51 -0300

    Seeder admin

---
+++ packages/backend/database/seeders/AdminSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Administrador',
+            'email' => 'admin@teste.com',
+            'password' => Hash::make('teste'),
+            'role' => 'admin',
+        ]); 
+    }
+}
